3db9775970afdadf771aa2872200663d4be63abb
suifengsigan <1075087594@qq.com>
2018-08-08 09:14:18 +0800

    1、新增面对角线

---
+++ FaceDiagonal/Business.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public partial class FaceDiagonalUI : SnapEx.BaseUI
+{
+    public Snap.NX.Face GetFace()
+    {
+        return selection0.SelectedObjects.FirstOrDefault() as Snap.NX.Face;
+    }
+    public override void Init()
+    {
+        selection0.AllowMultiple = false;
+        selection0.SetFilter(Snap.NX.ObjectTypes.Type.Face, Snap.NX.ObjectTypes.SubType.FacePlane);
+    }
+
+    public override void Apply()
+    {
+        var face = GetFace();
+        var uv = face.BoxUV;
+    }
+}
---
+++ FaceDiagonal/Unload.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FaceDiagonal
+{
+    public class Unload
+    {
+        public static void Main()
+        {
+            //EactBom.Unload.Main();
+        }
+
+        public static int GetUnloadOption(string arg)
+        {
+            //return System.Convert.ToInt32(Session.LibraryUnloadOption.Explicitly);
+            return 1;
+            // return System.Convert.ToInt32(Session.LibraryUnloadOption.AtTermination);
+        }
+    }
+}
