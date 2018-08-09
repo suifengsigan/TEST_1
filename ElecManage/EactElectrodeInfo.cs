5b010eb6de2b703a89c248b229529b41d6ff2aa3
suifengsigan <1075087594@qq.com>
2018-08-09 15:26:08 +0800

    1、优化

--- ElecManage/EactElectrodeInfo.cs
+++ ElecManage/EactElectrodeInfo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SnapEx;
 
 namespace ElecManage
 {
@@ -9,6 +10,105 @@ namespace ElecManage
     {
         public EactElectrodeInfo(Snap.NX.Body body) : base(body)
         {
+        }
+
+        public override int FINISH_NUMBER
+        {
+            get
+            {
+                var result = base.FINISH_NUMBER;
+                if (result == 0)
+                {
+                    result=_body.GetAttrIntegerValue(XKElecConst.ELE_F_COUNT);
+                }
+                return result;
+            }
+            set => base.FINISH_NUMBER = value;
+        }
+
+        public override int MIDDLE_NUMBER
+        {
+            get
+            {
+                var result = base.MIDDLE_NUMBER;
+                if (result == 0)
+                {
+                    result = _body.GetAttrIntegerValue(XKElecConst.ELE_S_COUNT);
+                }
+                return result;
+            }
+            set => base.MIDDLE_NUMBER = value;
+        }
+
+        public override int ROUGH_NUMBER
+        {
+            get
+            {
+                var result = base.ROUGH_NUMBER;
+                if (result == 0)
+                {
+                    result = _body.GetAttrIntegerValue(XKElecConst.ELE_R_COUNT);
+                }
+                return result;
+            }
+            set => base.ROUGH_NUMBER = value;
+        }
+
+
+        public override double FINISH_SPACE
+        {
+            get
+            {
+                var result = base.FINISH_SPACE;
+                if (result == 0)
+                {
+                    result = _body.GetAttrRealValue(XKElecConst.ELE_F_SG);
+                }
+                return result;
+            }
+            set => base.FINISH_SPACE = value;
+        }
+
+        public override double MIDDLE_SPACE
+        {
+            get
+            {
+                var result = base.MIDDLE_SPACE;
+                if (result == 0)
+                {
+                    result = _body.GetAttrRealValue(XKElecConst.ELE_S_SG);
+                }
+                return result;
+            }
+            set => base.MIDDLE_SPACE = value;
+        }
+
+        public override double ROUGH_SPACE
+        {
+            get
+            {
+                var result = base.ROUGH_SPACE;
+                if (result == 0)
+                {
+                    result = _body.GetAttrRealValue(XKElecConst.ELE_R_SG);
+                }
+                return result;
+            }
+            set => base.ROUGH_SPACE = value;
+        }
+
+        public override string MAT_NAME
+        {
+            get
+            {
+                var result = base.MAT_NAME;
+                if (string.IsNullOrEmpty(result))
+                {
+                    result = _body.GetAttrValue(XKElecConst.ELE_MATERIAL);
+                }
+                return result;
+            }
+            set => base.MAT_NAME = value;
         }
     }
 }
