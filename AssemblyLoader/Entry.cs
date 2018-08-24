fea4780ebada229a45cb30fb4ee9c8f15213bc5f
suifengsigan <1075087594@qq.com>
2018-08-24 15:21:34 +0800

    优化

--- AssemblyLoader/Entry.cs
+++ AssemblyLoader/Entry.cs
@@ -16,21 +16,27 @@ namespace AssemblyLoader
 
         private static Assembly MyResolveEventHandler(object sender, ResolveEventArgs args)
         {
-            Console.WriteLine("Resolving...");
-            var assemblbyName = args.Name.Split(',').FirstOrDefault();
-            var ufSession = NXOpen.UF.UFSession.GetUFSession();
-            NXOpen.UF.SystemInfo info;
-            ufSession.UF.AskSystemInfo(out info);
-            var version = "UG9.0";
-            if (info.program_name.Contains("NX 6")
-                ||info.program_name.Contains("NX 7")
-                )
+            var assemblyName = new AssemblyName(args.Name);
+            if (assemblyName.Name.Contains("PHSnap"))
             {
-                version = "UG6.0";
+                Console.WriteLine("Resolving...");
+                var assemblbyName = args.Name.Split(',').FirstOrDefault();
+                var ufSession = NXOpen.UF.UFSession.GetUFSession();
+                NXOpen.UF.SystemInfo info;
+                ufSession.UF.AskSystemInfo(out info);
+                var version = "UG9.0";
+                if (info.program_name.Contains("NX 6")
+                    || info.program_name.Contains("NX 7")
+                    )
+                {
+                    version = "UG6.0";
+                }
+                var path = string.Format("{0}\\SnapDll\\{1}", AppDomain.CurrentDomain.BaseDirectory, version);
+                var file = System.IO.Directory.GetFiles(path).FirstOrDefault(u => u == string.Format("{0}.dll", System.IO.Path.Combine(path, assemblbyName)));
+                return Assembly.LoadFile(file);
             }
-            var path = string.Format("{0}\\SnapDll\\{1}", AppDomain.CurrentDomain.BaseDirectory, version);
-            var file = System.IO.Directory.GetFiles(path).FirstOrDefault(u => u==string.Format("{0}.dll",System.IO.Path.Combine(path,assemblbyName)));
-            return Assembly.LoadFile(file);
+
+            return null;
         }
     }
 }
