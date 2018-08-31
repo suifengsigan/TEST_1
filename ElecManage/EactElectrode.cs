3dd070ff89dfee6c9f3c860bb28c20a61f650ad4
suifengsigan <1075087594@qq.com>
2018-08-31 13:23:30 +0800

    优化

--- ElecManage/EactElectrode.cs
+++ ElecManage/EactElectrode.cs
@@ -61,5 +61,62 @@ namespace ElecManage
             }
             return result;
         }
+
+        public override Snap.NX.Face GetChamferFace()
+        {
+            if (ChamferFace != null) return ChamferFace;
+            Snap.NX.Face result = null;
+            var results = new List<Snap.NX.Face>();
+            if (BaseFace != null)
+            {
+                var baseFOriention = new Snap.Orientation(BaseFace.GetFaceDirection());
+                var v1 = Snap.Vector.Unit(baseFOriention.AxisX);
+                var v2 = Snap.Vector.Unit(baseFOriention.AxisY);
+                var v3 = Snap.Vector.Unit(v1 + v2);
+                var v4 = v3.Copy(Snap.Geom.Transform.CreateRotation(new Snap.Position(), BaseFace.GetFaceDirection(), 90)).Unitize();
+
+                var chamferFaces = ElecBody.Faces.Where(u => u.ObjectSubType == Snap.NX.ObjectTypes.SubType.FacePlane && Snap.Compute.Distance(u, BaseFace) < SnapEx.Helper.Tolerance).ToList();
+                chamferFaces.ForEach(u =>
+                {
+                    var faceDir = u.GetFaceDirection();
+                    if (SnapEx.Helper.Equals(v3, faceDir, SnapEx.Helper.Tolerance)
+                        || SnapEx.Helper.Equals(-v3, faceDir, SnapEx.Helper.Tolerance)
+                        || SnapEx.Helper.Equals(-v4, faceDir, SnapEx.Helper.Tolerance)
+                        || SnapEx.Helper.Equals(v4, faceDir, SnapEx.Helper.Tolerance)
+                        )
+                    {
+                        results.Add(u);
+                    }
+                });
+            }
+
+            result = results.FirstOrDefault();
+
+            if (results.Count > 1)
+            {
+                var baseFaceBoxUV = BaseFace.BoxUV;
+                var elecBasePos = BaseFace.Position((baseFaceBoxUV.MinU + baseFaceBoxUV.MaxU) / 2, (baseFaceBoxUV.MinV + baseFaceBoxUV.MaxV) / 2);
+
+                var faceDirection = BaseFace.GetFaceDirection();
+                var plane = new Snap.Geom.Surface.Plane(elecBasePos, faceDirection);
+
+                foreach (var u in results)
+                {
+                    var uv = u.BoxUV;
+                    var cneterPoint = u.Position((uv.MaxU + uv.MinU) / 2, (uv.MaxV + uv.MaxV) / 2);
+                    var resullt = Snap.Compute.ClosestPoints(cneterPoint, plane);
+                    var dir = Snap.Vector.Unit(resullt.Point1 - resullt.Point2);
+                    if (SnapEx.Helper.Equals(dir, -faceDirection) && Snap.Compute.Distance(BaseFace, u) < SnapEx.Helper.Tolerance)
+                    {
+                        result = u;
+                        break;
+                    }
+                }
+            }
+
+            ChamferFace = result;
+
+            return result;
+        }
     }
 }
