421ce692a1ccdae9feda624fe456d3a814a1693b
suifengsigan <1075087594@qq.com>
2018-11-16 13:45:35 +0800

    优化

---
+++ CommonInterface/IEDM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CommonInterface
+{
+    /// <summary>
+    /// EDM图纸接口
+    /// </summary>
+    public interface IEDM
+    {
+        void CreateDrawingSheet(List<ElecManage.PositioningInfo> ps, Snap.NX.Body steel);
+    }
+
+    public abstract class FtpHelper
+    {
+        public static void FtpUpload(string type, ElecManage.MouldInfo steelInfo, string fileName, string partName,EactConfig.ConfigData ConfigData)
+        {
+            var EACTFTP = FlieFTP.Entry.GetFtp(ConfigData.FTP.Address, "", ConfigData.FTP.User, ConfigData.FTP.Pass, false);
+            string sToPath = string.Format("{0}/{1}/{2}", type, steelInfo.MODEL_NUMBER, partName);
+            switch (ConfigData.FtpPathType)
+            {
+                case 1:
+                    {
+                        var extension = Path.GetExtension(fileName).ToUpper();
+                        if (extension.Contains("STP") || extension.Contains("TXT"))
+                        {
+
+                        }
+                        else
+                        {
+                            sToPath = string.Format("{0}/{1}", type, steelInfo.MODEL_NUMBER, partName);
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        sToPath = string.Format("{0}/{1}", type, steelInfo.MODEL_NUMBER, partName);
+                        break;
+                    }
+            }
+            if (!EACTFTP.DirectoryExist(sToPath))
+            {
+                EACTFTP.MakeDirPath(sToPath);
+            }
+
+            EACTFTP.NextDirectory(sToPath);
+            EACTFTP.UpLoadFile(fileName);
+        }
+    }
+}
