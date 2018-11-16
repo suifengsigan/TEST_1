f2c42d1586469a95ad48067f7fa7025dd9e65d8d
suifengsigan <1075087594@qq.com>
2018-11-16 10:17:59 +0800

    优化

---
+++ ElecManage/CommonInterface/IEDM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonInterface
+{
+    /// <summary>
+    /// EDM图纸接口
+    /// </summary>
+    public interface IEDM
+    {
+        void CreateDrawingSheet(List<ElecManage.PositioningInfo> ps, Snap.NX.Body steel);
+    }
+}
